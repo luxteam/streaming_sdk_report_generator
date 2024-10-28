18ebf44aae4edc9bc430c14b42ac2415100b699a
kirilldkn <kirilldkn@gmail.com>
2024-10-28 16:54:36 +0100

    report_upd_test11

--- confluence_export.py
+++ confluence_export.py
@@ -57,9 +57,8 @@ def _request_confluence_report(report_date: datetime) -> html.Element:
         exit(-1)
 
     page_content = response.json()["results"][0]["body"]["storage"]["value"]
-    soup = BeautifulSoup(page_content, 'html.parser')
-    table = soup.get_text()
-    return table
+    soup = BeautifulSoup(page_content, 'lxml')
+    return soup
 
 
 def get_project_status(report_date: datetime):
