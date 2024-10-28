fd979d804bacdb3258b6879ef44ddf361d978c70
kirilldkn <kirilldkn@gmail.com>
2024-10-28 14:54:04 +0100

    report_upd_test4

--- confluence_export.py
+++ confluence_export.py
@@ -68,7 +68,7 @@ def get_project_status(report_date: datetime):
     summary = []
     planned = []
 
-    summary_ul, planned_ul = page.str(
+    summary_ul, planned_ul = page.xpath(
         '//p/span[text()="StreamingSDK:"]/parent::p/following-sibling::ul'
     )
 
