2754b760d197d15e4bf9b7010c612e16f99528ff
kirilldkn <kirilldkn@gmail.com>
2024-10-28 21:29:00 +0100

    report_upd_test37

--- confluence_export.py
+++ confluence_export.py
@@ -68,7 +68,7 @@ def get_project_status(report_date: datetime):
 
     data = []
     table_body = page.find('tbody')
-    rows = table_body.find_next_sibling(string=re.compile("SSDK:"))
+    rows = table_body.find_all_next(string=re.compile("SSDK:"))
     for row in rows:
         cell_data = row.get_text(strip=True)
         data.append(cell_data)
