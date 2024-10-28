962941dbe8e5b19500515d090aad8bc73df6b50b
kirilldkn <kirilldkn@gmail.com>
2024-10-28 17:41:47 +0100

    report_upd_test17

--- confluence_export.py
+++ confluence_export.py
@@ -59,7 +59,7 @@ def _request_confluence_report(report_date: datetime) -> html.Element:
     page_content = response.json()["results"][0]["body"]["storage"]["value"]
     soup = BeautifulSoup(page_content, 'lxml')
     data =[]
-    table = soup.find(id='main-content')
+    table = soup.find(id='main-content',class="wiki-content")
     table_body = table.find('tbody')
     rows = table_body.find_all('tr')
     for row in rows:
