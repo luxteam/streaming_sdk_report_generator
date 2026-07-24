9a60431c38cac60d5bbfc7f563f63fae361fd516
kirilldkn <kirilldkn@gmail.com>
2026-07-24 12:57:59 +0200

    mail_time_back

--- gen_emails.py
+++ gen_emails.py
@@ -139,7 +139,7 @@ def generate_first_letter(
                 continue
 
             since_date = (
-                datetime.today() - timedelta(weeks=2) + timedelta(days=2)
+                datetime.today() - timedelta(weeks=1) + timedelta(days=1)
             ).replace(hour=0, minute=0, second=0, microsecond=0)
 
             latest_report = get_latest_report(job, report, newer_than=since_date)
